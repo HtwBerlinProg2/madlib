b85e1991c05af13282862f4cd3850055e37d94d4
Rame Madah <lawkira10@gmail.com>
2021-05-15 01:04:03 +0200

    test update 0.3 done

--- src/test/java/de/htwberlin/madlib/MadLibTest.java
+++ src/test/java/de/htwberlin/madlib/MadLibTest.java
@@ -14,17 +14,28 @@ public class MadLibTest {
     static final String[] VERBS = new String[]{"concentrate", "compete", "battle", "cry", "develop", "dance", "destroy", "dream", "sleep"};
     static final String[] FAMOUS_PERSONS = new String[]{"Frodo", "Voldemort", "John Wick", "Winnie Pooh", "Thor", "Iron Man", "Hulk", "Supergirl", "Wonder Woman"};
 
+    boolean checker (String sen ,String[] spl){
+        for(String i : spl)
+            if (sen.contains(i)){
+                return true;
+            }
+        return false;
+    }
+
+
     @Test
     @DisplayName("should check the inserted variables")
     public void createTest() {
 
         MadLib p = new MadLib();
-        String expected = "Java programming is so "+ ADJECTIVES +"! It makes me so excited " +
-                "all the time because I love to "+ VERBS +"." +
-                " Stay hydrated and "+ VERBS +" like you are "+ FAMOUS_PERSONS +"!\n";
+
+
         String actual = p.create();
-        assertEquals(expected, actual);
+        assertTrue(checker(actual, MadLibTest.ADJECTIVES));
+        assertTrue(checker(actual, MadLib.VERBS));
+        assertTrue(checker(actual, MadLib.FAMOUS_PERSONS));
     }
+
 
 
     @Test
@@ -33,7 +44,7 @@ public class MadLibTest {
         MadLib p = new MadLib();
         String expected = "Java programming is so ";
         String actual = p.create();
-        assertTrue(actual != null && actual.contains(expected));
+        assertTrue(actual != null && actual.startsWith("Java programming is so "));
     }
 
 }
